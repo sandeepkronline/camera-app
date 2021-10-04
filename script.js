4d8ea1e5018817a4adef300efcadd803b8a2f19a
Sandeep KR <sandeep.kr@trenser.com>
2021-10-04 13:30:40 +0900

    Initial commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const cameraView = document.querySelector("#camera--view"),
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
-        .getUserMedia(constraints)
+        .getUserMedia({video: true})
         .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
